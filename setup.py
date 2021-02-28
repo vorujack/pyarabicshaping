e1bdd57f2e59c4993548acb9f61cc0d38b71b06e
vorujack <esmaeil.mohammadi@hezardastan.net>
2021-02-28 20:47:25 +0330

    create packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from distutils.core import setup
+setup(
+    name="pyarabicshaping",
+    version="1.0",
+    packages=find_packages(),
+    author="vorujack",
+    author_email="vorujack@gmail.com",
+    description="arabic shapes",
+    license="MIT",
+    url='https://github.com/vorujack/pyarabicshaping',
+    download_url='https://github.com/vorujack/pyarabicshaping/tarball/1.0',
+    keywords="arabic",
+)
